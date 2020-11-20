9758e34a742660083b93bd43fdd237c38726ca79
IbrahimMohammed47 <ibrahim.mohammed.4748@gmail.com>
2020-11-20 15:26:40 +0200

    seed

---
+++ inquirier.js
@@ -0,0 +1,48 @@
+const inquirer = require('inquirer');
+
+module.exports = {
+  askGUCCredentials: () => inquirer.prompt([
+    {
+      name: 'email',
+      type: 'input',
+      message: 'Enter your guc email address:',
+      validate: function (value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'fen el email ya liss';
+        }
+
+      },
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function (value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'etla3 bel password now';
+        }
+      }
+    }
+  ]),
+  showMainMenu: () => inquirer.prompt([
+    {
+      name: 'mainMenu',
+      type: 'list',
+      message: 'Main Menu',
+      choices: ["login", "tools", "exit"]
+    }
+  ]),
+  showToolsMenu: () => inquirer.prompt([
+    {
+      name: 'toolsMenu',
+      type: 'list',
+      message: 'Tools Menu',
+      choices: ["cms_downloader", "evaluation_roaster", "back"]
+    }
+  ])
+
+}
